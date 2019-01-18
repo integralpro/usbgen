dafb927de986b43bde06fdb53cb7cef775a76a66
pavel_pro <pavel_pro@pgp01.io>
2019-01-18 00:01:54 -0800

    Fixed cmake dependencies

--- include/Std.h
+++ include/Std.h
@@ -1,13 +1,4 @@
-//
-// Created by Pavel Prokofyev on 2019-01-03.
-//
-
-#ifndef __USBGEN_STD__
-#define __USBGEN_STD__
-
 #include <array>
 #include <cstdint>
 #include <numeric>
 #include <tuple>
-
-#endif //__USBGEN_STD__
